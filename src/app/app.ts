849e386e5eb90f68c0573d5f585cd2ead3cfcebc
Emerson Leite <emersonleite@galileosoft.com.br>
2025-08-16 11:11:39 -0300

    refactor: move layout to core directory.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from "./layout/header/header";
+import { Header } from "./core/layout/header/header";
 
 @Component({
   selector: 'app-root',
