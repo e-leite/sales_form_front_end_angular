4e41f82e15db8fabd36a976f80cf285f25c82582
Emerson Leite <emersonleite@galileosoft.com.br>
2025-08-16 13:16:31 -0300

    feat: create SidebarService.

---
+++ src/app/core/services/sidebar-service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { SidebarMenuItem } from '../models/sidebar-menu-item';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SidebarService {
+    private sidebarMenuItems: SidebarMenuItem[] = [
+        { label: 'Home', route: '', icon: 'home' },
+        { label: 'Teams', route: '', icon: 'groups' },
+    ];
+
+    getSidebarMenuItems(): Observable<SidebarMenuItem[]> {
+        return of(this.sidebarMenuItems);
+    }  
+}
