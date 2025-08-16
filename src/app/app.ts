7774a53a58f9193bd6cf41bbe06e859841565a6d
Emerson Leite <emersonleite@galileosoft.com.br>
2025-08-16 10:45:49 -0300

    feat: create header component.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./layout/header/header";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('sales_form_front_end');
+  protected readonly title = signal('Sales Form Sender');
 }
