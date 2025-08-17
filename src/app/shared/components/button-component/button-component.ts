3307135421d5926809259e0654d00348c9897bf4
Emerson Leite <emersonleite@galileosoft.com.br>
2025-08-17 19:17:15 -0300

    feat: add handleClick method.

--- src/app/shared/components/button-component/button-component.ts
+++ src/app/shared/components/button-component/button-component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -10,4 +10,9 @@ import { CommonModule } from '@angular/common';
 export class ButtonComponent {
     @Input() label: string = 'Button';
     @Input() type: 'primary' | 'success' | 'error' = 'primary';
+    @Output() clicked = new EventEmitter<void>();
+
+    handleClick() {
+        this.clicked.emit();
+    }
 }
