d60bef233269b54d58734ee96cc4c40338b63346
Emerson Leite <emersonleite@galileosoft.com.br>
2025-08-17 19:15:06 -0300

    feat: add saveSalesTeam method.

--- src/app/services/sales-team-service.ts
+++ src/app/services/sales-team-service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ISalesTeam } from '../shared/interfaces/sales-team.interface';
+import { SalesTeamCreateDto } from '../shared/dtos/sales-team-create-dto';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +15,9 @@ export class SalesTeamService {
 
     getSalesTeam(): Observable<ISalesTeam[]> {
         return this.httpClient.get<ISalesTeam[]>(`${this.apiUrl}/salesteams`)
-    }  
+    }
+
+    saveSalesTeam(salesTeamCreateDto: SalesTeamCreateDto): Observable<ISalesTeam> {
+        return this.httpClient.post<ISalesTeam>(`${this.apiUrl}/salesteams`, salesTeamCreateDto)
+    }
 }
