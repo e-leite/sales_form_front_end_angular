a50982d9eafe0eee80986a152f6f533794493092
Emerson Leite <emersonleite@galileosoft.com.br>
2025-08-28 18:18:00 -0300

    feat: create SkillCategoryService.

---
+++ src/app/skill-category/services/skill-category-service.ts
@@ -0,0 +1,36 @@
+import { inject, Injectable } from '@angular/core';
+import { ISkillCategory } from '../../shared/interfaces/skill-category.interface';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ProgramingLanguageCreateDto } from '../../shared/dtos/programing-language-create-dto';
+import { SkillCategoryCreateDto } from '../../shared/dtos/skill-category-create-dto';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SkillCategoryService {
+    private readonly httpClient = inject(HttpClient);
+
+    private readonly apiUrl = '/api/skillcategories';
+
+    get(): Observable<ISkillCategory[]> {
+        return this.httpClient.get<ISkillCategory[]>(`${this.apiUrl}`)
+    }
+
+    getById(id: string): Observable<ISkillCategory> {
+        return this.httpClient.get<ISkillCategory>(`${this.apiUrl}/${id}`)
+    }
+
+    save(dto: SkillCategoryCreateDto): Observable<ISkillCategory> {
+        return this.httpClient.post<ISkillCategory>(`${this.apiUrl}`, dto)
+    }
+
+    update(id: string, dto: SkillCategoryCreateDto): Observable<ISkillCategory> {
+        return this.httpClient.put<ISkillCategory>(`${this.apiUrl}/${id}`, dto)
+    }
+
+    delete(id: string): Observable<void> {
+        return this.httpClient.delete<void>(`${this.apiUrl}/${id}`)
+    }
+  
+}
