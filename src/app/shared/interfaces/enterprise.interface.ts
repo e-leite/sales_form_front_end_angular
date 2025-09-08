1874fd05b3f26f90fa45c1b22d52a0dfa6d0c4d2
Emerson Leite <emersonleite@galileosoft.com.br>
2025-09-08 13:15:33 -0300

    feat: create IEnterprise.

---
+++ src/app/shared/interfaces/enterprise.interface.ts
@@ -0,0 +1,12 @@
+export interface IEnterprise {
+    id: string,
+    name: string,
+    overview: string,
+    site: string,
+    industry: string,
+    companySize: string,
+    foundationYear: number,
+    createdAt: string,
+    updatedAt: string
+
+}
