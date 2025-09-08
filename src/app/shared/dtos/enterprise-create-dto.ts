5f95be783b51d3e600d066e614d5d8dec2eb57c3
Emerson Leite <emersonleite@galileosoft.com.br>
2025-09-08 13:15:54 -0300

    feat: create EnterpriseCreateDto.

---
+++ src/app/shared/dtos/enterprise-create-dto.ts
@@ -0,0 +1,8 @@
+export interface EnterpriseCreateDto {
+    name: string, 
+    overview: string, 
+    site: string, 
+    industry: string, 
+    companySize: string, 
+    foundationYear: number
+}
